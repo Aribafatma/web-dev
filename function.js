2e58819c10c30a8e51cddd854a8b8fee74089a68
Aribafatma <118280104+Aribafatma@users.noreply.github.com>
2026-03-26 08:43:18 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,18 @@
+//function sum(a,b){
+   // return a+b;
+//}
+//let ans = sum(2,3)
+//console.log(sum);
+function sum(n){
+    let ans =0;
+    for(let i = 1; i<= n; i++){
+        ans = ans +1
+    }
+    return ans;
+}
+const ans = sum(100);
+console.log(ans);
+const ans2 = sum(1000);
+console.log(ans2);
+const ans3 = sum(1000);
+console.log(ans3);
